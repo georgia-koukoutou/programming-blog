f78f8cb5e37416b53913ab4d82002f5bfb06a08b
Georgia Koukoutou <georgia.koukoutou@gmail.com>
2022-03-18 18:28:39 +0200

    Updated about page and database URI

--- application/config.py
+++ application/config.py
@@ -2,5 +2,7 @@ import os
 
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "c434c9b7cf370b5f5f3f13a741bd9fab"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost/blog"
+
